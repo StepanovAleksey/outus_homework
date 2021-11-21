f62869ac64e4f4c4c65c785ca0186058afee70e9
Aleksey Stepanov <bssClient@gmail.com>
2021-11-21 21:33:13 +0500

    remove comments

--- src/home_2/childProcess.ts
+++ src/home_2/childProcess.ts
@@ -66,40 +66,7 @@ class ChildWorkerModel extends BaseWorkerModel {
       new HandleCodeCommand(),
       new HandleSystemCommand()
     );
-    // this.sendParentCommand(new SystemCommand(ESystemCommandType.isStarted));
   }
-
-  // protected handleInfoCommand(command: ICommand<string>) {
-  //   console.info("child process info:", command.payload);
-  // }
-
-  // protected handleErrorCommand(command: ICommand<string>) {
-  //   console.error("child process error:", command);
-  // }
-
-  // protected handleSystemCommand(command: ICommand<ESystemCommandType>) {
-  //   switch (command.payload) {
-  //     case ESystemCommandType.softStop:
-  //       this.isSoftStop = true;
-  //       this.parentCmpleteCommandMessage(command.uid);
-  //       process.exit(0);
-  //   }
-  // }
-
-  // protected handleCodeCommand(command: ICommand<string>) {
-  //   eval(command.payload);
-  // }
-  // protected sendParentCommand(command: ICommand) {
-  //   parentPort.postMessage(command);
-  // }
-
-  // protected evolveCommand() {
-  //   try {
-  //     super.evolveCommand();
-  //   } catch (error) {
-  //     this.sendParentCommand(new ErrorCommand(error as string));
-  //   }
-  // }
 }
 
 const childWorker = new ChildWorkerModel();
